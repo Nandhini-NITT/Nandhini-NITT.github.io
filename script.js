264def0bcaafbf3556ff2b06e92d07771c3dee2e
Nandhini <nandhini.devi1998@gmail.com>
2016-06-02 20:00:47 +0530

    Modified journey-to-nitt.html.Bug fix and Add extra features to Memory-game.html.
    Image captions for slideshow made bold in journey-to-nitt.html.Bug Fix:tile made unclickable once matched.Number of moves and score displayed at the end of game with alert.Timer is set with value 3 minutes(fixed).

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 window.onload=function()
 {
-random_assign();
 new_board();
+timer_initialiser();
 }
+var sec_count,min_count,stop,timerPause=0;
 function new_board()
 {
+random_assign();
 var table = document.getElementById("game-board");
 if (table != null) 
 {
@@ -14,22 +16,67 @@ if (table != null)
 		{
 			table.rows[i].cells[j].onclick = function () 
 				{
-				memoryFlipTile(this);
+				if(timerPause==0)
+				{
+					moves++;
+					memoryFlipTile(this);
+				}
 				}
 			table.rows[i].cells[j].innerHTML="<img src='tile.jpg'><p id='number'>"
 		}
 	}
 }
 }
+function pause_timer()
+{
+clearInterval(stop);
+timerPause=1;
+}
+function timer_initialiser()
+{	
+	sec_count=0;
+	min_count=3;
+	document.getElementById("min").innerHTML="0"+3;
+	document.getElementById("sec").innerHTML="0"+0;
+	main_timer();
+}
+function replay()
+{
+	timer_initialiser();
+	score=0;
+	moves=0;
+	new_board();
+}
+function main_timer()
+{
+timerPause=0;	
+clearInterval(stop);
+stop=setInterval(function(){
+if(sec_count==0 && min_count>0)
+	{
+	min_count-=1;
+	sec_count=59;
+	}
+else if(min_count==0 && sec_count==0)
+	{
+	alert("Game Over... Score "+score+" number of moves"+moves/2);
+	replay();
+	}
+else 
+sec_count--;
+document.getElementById("min").innerHTML=(min_count>=0 && min_count<10)?('0'+min_count):min_count;
+document.getElementById("sec").innerHTML=(sec_count>=0 && sec_count<10)?('0'+sec_count):sec_count;
+},1000);
+}
 
 
 var flag=0;
-var possible="ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+var possible="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var memory_values=[];
 var memory_tile_ids=[];
 var arr=[];
 var memory_random=[];
-var tiles_flipped=0;
+var tiles_flipped=0,moves=0,score=0;
 
 function memoryFlipTile(tile)
 {	
@@ -49,16 +96,25 @@ function memoryFlipTile(tile)
 			memory_values.push(memory_random[b]);
 			memory_tile_ids.push(tile.id);
 				if(memory_values[0] == memory_values[1] && memory_tile_ids[0]!=memory_tile_ids[1])
-				{
+				{	score++;
 					tiles_flipped += 2;
 				// Clear both arrays
 					memory_values = [];
+					
 					memory_tile_ids = [];
+					
+						tile.onclick = function () 
+						{
+						;
+						}
+					
+							
 				// Check to see if the whole board is cleared
 					if(tiles_flipped == arr.length)
 					{
-						alert("Board cleared... generating new board");
+						alert("Board cleared... Score"+score+" number of moves"+moves/2);
 						new_board();
+						timer_initialiser();
 
 					}
 				}	 
